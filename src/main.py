14e145404e25cfd6a4c941c4babaa0c25367688a
Muhammad Mahad <mahadtxt@gmail.com>
2024-09-28 19:03:39 +0000

    Refactor substitute_nibbles function to use a separate module
    The substitute_nibbles function in main.py has been refactored to use a separate module called lib.py.

--- src/main.py
+++ src/main.py
@@ -3,49 +3,7 @@ Implementation of a custom AES algorithm variant named 'Pocket Algorithm'
 developed for my Information Security course.
 """
 
-
-def substitute_nibbles(plainTextBinary: str) -> list:
-    """
-    substituting each nibble in the block with a different one
-    """
-    # From the given document
-    sBox: dict = {
-        "0000": "1010",
-        "0001": "0000",
-        "0010": "1001",
-        "0011": "1110",
-        "0100": "0110",
-        "0101": "0011",
-        "0110": "1111",
-        "0111": "0101",
-        "1000": "0001",
-        "1001": "1101",
-        "1010": "1100",
-        "1011": "0111",
-        "1100": "1011",
-        "1101": "0100",
-        "1110": "0010",
-        "1111": "1000",
-    }
-
-    subNibbles: list[str] = list()
-    if len(plainTextBinary) == 4:
-        # input is a 4 bit nibble
-        subNibbles.append(sBox[plainTextBinary])
-    elif len(plainTextBinary) == 16:
-        # splitting into 4 bit nibbles
-        for i in range(0, 16, 4):
-            subNibbles.append(sBox[plainTextBinary[i : i + 4]])
-    else:
-        raise ValueError("Invalid must be either 4 or 16 bits long")
-
-    hexValues: list = list()
-
-    for binaryValue in subNibbles:
-        hexValue = hex(int(binaryValue, 2))[2:]
-        hexValues.append(hexValue)
-
-    return hexValues
+from lib import substitute_nibbles
 
 
 def main() -> None:
