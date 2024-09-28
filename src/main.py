6a666f6e45ef0e516793317826e44131f4a5d7de
Muhammad Mahad <mahadtxt@gmail.com>
2024-09-28 18:05:34 +0000

    setup: Add main and `secret.txt`

---
+++ src/main.py
@@ -0,0 +1,12 @@
+""" 
+Implementation of a custom AES algorithm variant named 'Pocket Algorithm'
+developed for my Information Security course.
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
