a52602a117d6eecb676fa02976ad17d7be0c6493
Muhammad Mahad <mahadtxt@gmail.com>
2024-09-29 15:55:24 +0000

    Done d3

--- src/d3.py
+++ src/d3.py
@@ -1,4 +1,47 @@
 from d2 import perform_decryption
+from lib import bitwise_xor, substitute_nibbles, gen_round_keys, mix_columns, shift_rows
+
+
+def perform_encryption(block: str, key: str) -> str:
+    """
+    Perform encryption
+    """
+    # Round 1
+
+    # SubNibbles
+    substitueNibbles = substitute_nibbles(block)
+    substitueNibbles = "".join(substitueNibbles)
+    substitueNibbles = bin(int(substitueNibbles, 16))[2:].zfill(16)
+    rk1, rk2 = gen_round_keys(key)
+    rk1 = "".join(rk1)
+    rk2 = "".join(rk2)
+
+    # AddRoundKey
+    rk1_xor_subNibbles = bitwise_xor(substitueNibbles, rk1)
+    # MixCols
+    mixCols = mix_columns(rk1_xor_subNibbles)
+    mixCols = "".join(mixCols)
+    mixCols = bin(int(mixCols, 16))[2:].zfill(16)
+    # ShiftRows
+    shiftRows = shift_rows(mixCols)
+    shiftRows = "".join(shiftRows)
+    shiftRows = bin(int(shiftRows, 16))[2:].zfill(16)
+
+    # Round 2
+    # SubNibbles
+    substitueNibbles = substitute_nibbles(shiftRows)
+    substitueNibbles = "".join(substitueNibbles)
+    substitueNibbles = bin(int(substitueNibbles, 16))[2:].zfill(16)
+    # Add rk2
+    rk2_xor_subNibbles = bitwise_xor(substitueNibbles, rk2)
+    # shiftRows
+    shiftRows = shift_rows(rk2_xor_subNibbles)
+    # the result is already in hex format no need to convert it
+
+    cipherText = "".join(shiftRows)
+    return cipherText
+    # print(f"Debugging: {cipherText}")
+    # exit()
 
 
 def encryption():
@@ -12,9 +55,24 @@ def encryption():
         plainText += "\0"
     key = "149c"
     plainTextHex = plainText.encode("utf-8").hex()
+    # Making a list four hex values to perform encryption with ease
+    plainTextHexList = list()
+    for i in range(0, len(plainTextHex), 4):
+        plainTextHexList.append(plainTextHex[i : i + 4])
+
+    # print(f"Plain Text: {plainTextHexList}")
+
+    cipherText = str()
+    for block in plainTextHexList:
+        binaryBlock = bin(int(block, 16))[2:].zfill(16)
+        binaryKey = bin(int(key, 16))[2:].zfill(16)
+        # print(f"Block: {binaryBlock}")
+        # print(f"Block: {binaryKey}")
+        cipherText += f"{perform_encryption(binaryBlock, binaryKey)} "
     # reverse of encode
-    plainTextHex = bytes.fromhex("4765").decode("utf-8")
-    print("Plain Text in Hex: ", plainTextHex)
+    # plainTextHex = bytes.fromhex(plainTextHex).decode("utf-8")
+    # print("Plain Text in Hex: ", plainTextHex)
+    print(f"Cipher Text: {cipherText}")
 
 
 def main():
@@ -39,4 +97,5 @@ def main():
 
 
 if __name__ == "__main__":
+    encryption()
     main()
